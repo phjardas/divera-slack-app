3773890793d6de50319fa698bcdc2b2f1d1e495c
Philipp Jardas <philipp@jardas.de>
2023-08-03 13:27:37 +0200

    remove serverless deployment

--- src/lambdas/test.ts
+++
@@ -1,7 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ message: "Hello World!" }),
-  };
-}
